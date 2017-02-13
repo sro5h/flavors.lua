2403a64de7eadbe82eb6aab84d95430a8f7ff4eb
Paul Meffle <meffle.paul@me.com>
2017-02-13 12:38:37 +0100

    Add documentation comments

--- flavors.lua
+++ flavors.lua
@@ -9,19 +9,35 @@
 -- **Source:** [Github](https://github.com/sro5h/flavors.lua)
 --
 
+-- ## Modules
+--
 local Json = require("JSON")
 local File = require("file")
 
--- help string
+-- ### HELP
+--
+-- The help string.
+--
 local HELP = [[
 Usage: flavors.lua [flavor]
     [flavor]:
         Needs to be a subdir containing a flavor.json
 ]]
 
--- constants
+-- ### FILE_FLAVOR
+--
+-- The local configuration file's name.
+--
 local FILE_FLAVOR = "flavor.json"
 
+-- ### copyFiles
+--
+-- Function to copy all 'files' from 'src' to 'dest'.
+--
+-- - 'src'   is a string
+-- - 'dest'  is a string
+-- - 'files' is a table
+--
 function copyFiles(src, dest, files)
     for _, file in pairs(files) do
         -- if no dest name is specified use src
@@ -30,6 +46,12 @@ function copyFiles(src, dest, files)
     end
 end
 
+-- ## applyFlavor
+--
+-- Applies the specified flavor.
+--
+-- - 'flavor' is a string
+--
 function applyFlavor(flavor)
     -- check if flavor exists
     if File.exists(flavor .. "/" .. FILE_FLAVOR) then
